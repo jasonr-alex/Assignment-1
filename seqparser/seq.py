97fa86052af303fd747ff93ba00022a379fe685e
Jason Rivera <jasonrivera.alex@gmail.com>
2024-10-24 06:49:33 -0700

    Added changes to reverse transcriptase function

--- seqparser/seq.py
+++ seqparser/seq.py
@@ -6,12 +6,6 @@ def transcribe(seq: str) -> str:
     return ''.join([dna_to_rna[base] for base in seq])
 
 def reverse_transcribe(seq: str) -> str:
-    """
-    TODO: transcribes DNA to RNA then reverses
-    the strand
-    """
-    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A', 'N': 'N'}
-    t = ''
-    for base in s:
-        t = complement[base] + t
-    return t
+    dna_to_rna = {'A': 'U', 'T': 'A', 'C': 'G', 'G': 'C', 'N': 'N'}  # N represents any unknown base
+    rna_sequence =  ''.join([dna_to_rna[base] for base in seq])
+    return rna_sequence[::-1]
